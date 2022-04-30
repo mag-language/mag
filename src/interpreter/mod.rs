4834b0ac7fe1c3be9460e42db82dcd11fbb217b7
Moritz Riede <moritz@mriede.com>
2022-04-30 14:15:45 +0200

    feat: add support for method definitions

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -22,11 +22,41 @@ impl Interpreter {
     pub fn evaluate(&mut self, expr: Box<Expression>) -> Result<Box<Expression>, InterpreterError> {
         match expr.clone().kind {
             ExpressionKind::Infix(infix) => self.evaluate_infix(infix),
+            ExpressionKind::Method(method) => self.evaluate_method(method),
             ExpressionKind::Literal(literal) => Ok(expr),
             ExpressionKind::Pattern(Pattern::Tuple { expr }) => Ok(self.evaluate(expr)?),
 
             _ => unimplemented!(),
         }
+    }
+
+    /// Add a new instance of a multimethod definition if it doesn't already exist.
+    fn evaluate_method(&mut self, method: Method) -> Result<Box<Expression>, InterpreterError> {
+        if let Some(mut multimethod) = self.multimethods.get_mut(&method.name) {
+            // Make sure this very definition does not already exist.
+            if let None = multimethod.receivers.get_mut(&method.signature) {
+                multimethod.receivers.insert(
+                    method.signature,
+                    method.body,
+                );
+            } else {
+                return Err(InterpreterError::MethodSignatureExists)
+            }
+        } else {
+            self.multimethods.insert(method.name.clone(), Multimethod::new(
+                method.signature,
+                method.body,
+            ));
+        }
+
+        println!("methods: {:#?}", self.multimethods);
+
+        Ok(Box::new(Expression {
+            kind: ExpressionKind::Identifier,
+            start_pos: 0,
+            end_pos: 0,
+            lexeme: method.name,
+        }))
     }
 
     fn evaluate_infix(&mut self, infix: Infix) -> Result<Box<Expression>, InterpreterError> {
@@ -83,10 +113,24 @@ impl Interpreter {
 #[derive(Debug, Clone, PartialEq)]
 pub enum InterpreterError {
     UnexpectedType,
+    MethodSignatureExists,
 }
 
 /// A method definition that has one name and many pairs of function signatures and bodies.
+#[derive(Debug)]
 pub struct Multimethod {
-    /// The individual methods this multimethod is composed of.
-    pub receivers: HashMap<Pattern, Box<Expression>>,
+    /// The individual signatures and bodies this multimethod is composed of.
+    pub receivers: HashMap<Box<Expression>, Box<Expression>>,
+}
+
+impl Multimethod {
+    pub fn new(signature: Box<Expression>, body: Box<Expression>) -> Self {
+        let mut receivers = HashMap::new();
+
+        receivers.insert(signature, body);
+
+        Self {
+            receivers,
+        }
+    }
 }
