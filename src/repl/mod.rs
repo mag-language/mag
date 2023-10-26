8ba6dc0f31ccdfa060447e2844c50ca1ad4959e8
Moritz Riede <moritz@mriede.com>
2023-10-26 03:55:31 +0200

    feat: integrate new compiler structure and fix compiler warnings

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -1,6 +1,5 @@
 use crossterm::{
     event::{self, Event, KeyCode, KeyEvent},
-    cursor,
     Result,
 };
 
@@ -13,8 +12,8 @@ use super::runtime::Runtime;
 pub struct Repl {
     runtime: Runtime,
     cursor_x: usize,
-    history: Vec<String>,
-    history_y: usize,
+    _history: Vec<String>,
+    _history_y: usize,
 }
 
 impl Repl {
@@ -22,29 +21,37 @@ impl Repl {
         Self {
             runtime: Runtime::new(),
             cursor_x: 0,
-            history: vec![],
-            history_y: 0,
+            _history: vec![],
+            _history_y: 0,
         }
     }
 
     pub fn launch(&mut self) -> Result<()> {
-        let mut should_continue = true;
+        println!("");
+        println!("{}", std::fs::read_to_string("./logo.txt")?);
+        let _should_continue = true;
 
-        while should_continue {
+        while _should_continue {
             // need to explicitly flush this to ensure it prints before read_line
             print!("{} ", ">>>".green().bold());
             stdout().flush()?;
 
             let line = self.read_line()?;
 
-            self.runtime.lexer.add_text(line);
+            /*self.runtime.lexer.add_text(line);
             let tokens = self.runtime.lexer.parse();
-            let tree = self.runtime.parser.add_tokens(self.runtime.lexer.source.clone(), tokens);
-            let result = self.runtime.parser.parse();
+            let _tree = self.runtime.parser.add_tokens(self.runtime.lexer.source.clone(), tokens);
+            let result = self.runtime.parser.parse();*/
+
+            let result = self.runtime.compiler.compile(line);
 
             match result {
-                Ok(r) => {
-                    println!("{:#?}", r);
+                Ok(instructions) => {
+                    // println!("{}\n{:#?}", "instructions:".bright_blue().bold(), instructions);
+
+                    for instruction in instructions {
+                        self.runtime.machine.execute(instruction).unwrap();
+                    }
                 },
                 Err(e) => {
                     println!("{} {:?}", "error:".bright_red().bold(), e);
