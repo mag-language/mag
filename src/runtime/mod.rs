1cdf42c41abd1a17d647b7573ff4bf636f003dbc
Moritz Riede <moritz@mriede.com>
2023-10-26 03:57:07 +0200

    feat: add compiler and virtual machine to runtime along with doc comments

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,9 +1,18 @@
 use magc::lexer::Lexer;
 use magc::parser::Parser;
+use magc::compiler::Compiler;
+use strontium::Strontium;
 
+/// A runtime instance, which contains all the data structures and methods needed to
+/// compile and run a program while keeping track of its state and reporting errors.
 pub struct Runtime {
+    /// Converts a source string into a linear sequence of tokens.
     pub lexer:  Lexer,
+    /// Assembles a sequence of tokens into a tree of expressions.
     pub parser: Parser,
+    /// Compiles the AST into a sequence of instructions.
+    pub compiler: Compiler,
+    pub machine: Strontium,
 }
 
 impl Runtime {
@@ -11,6 +20,8 @@ impl Runtime {
         Self {
             lexer:  Lexer::new(),
             parser: Parser::new(),
+            compiler: Compiler::new(),
+            machine: Strontium::new(),
         }
     }
 }
